424c29e18c79679e2bf5afc12fe1df352c7efee0
laj <larry.jones@reveal-energy.com>
2022-06-08 20:15:07 -0500

    Exploring the `Option` type

--- Ch03/option.fsx
+++ Ch03/option.fsx
@@ -14,9 +14,48 @@ open System
 let tryParseDateTime (input:string) =
     match DateTime.TryParse input with
     | true, result -> Some result
-    | _ -> None
+    | _ -> None 
 
 // Any of these three styles is "acceptable."
 
 let isDate = tryParseDateTime "2019-08-01"
 let isNotDate = tryParseDateTime "Hello"
+
+type PersonName = {
+    FirstName: string
+    MiddleName: string option
+    LastName: string
+}
+
+let person = { FirstName = "Ian"; MiddleName = None; LastName = "Russel" }
+printfn $"{person}"
+let person2 = { person with MiddleName = Some "???" }
+printfn $"{person2}"
+
+// Interop with .NET
+
+let nullObj:string = null
+let nullPri = Nullable<int>()
+
+let fromNullObj = Option.ofObj nullObj
+let fromNullPri = Option.ofNullable nullPri
+
+let toNullObj = Option.toObj fromNullObj
+let toNullPri = Option.toNullable fromNullPri
+
+let resultPM input =
+    match input with
+    | Some value -> value
+    | None -> "------"
+
+resultPM fromNullObj
+resultPM (Some "foo")
+
+let result = Option.defaultValue "------" fromNullObj
+
+// Use partial application to set a default value *without* an `Option`
+let setUnknownAsDefault = Option.defaultValue "????"
+
+setUnknownAsDefault fromNullObj
+let anotherResult = fromNullObj |> setUnknownAsDefault
+printfn $"{anotherResult}"
