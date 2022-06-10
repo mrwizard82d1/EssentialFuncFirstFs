630ab54218ac17d9e41f08bfdb717c21eb7a2a9e
laj <larry.jones@reveal-energy.com>
2022-06-10 18:22:10 -0500

    Generate template more consistent with book

--- Ch04/create_fs_solution.sh
+++ Ch04/create_fs_solution.sh
@@ -1,13 +1,13 @@
-dotnet new sln -o Ch04
-cd Ch04
+dotnet new sln -o MySolution
+cd MySolution
 mkdir src
-dotnet new console -lang F# -o src/Ch04
-dotnet sln add src/Ch04/Ch04.fsproj
+dotnet new console -lang F# -o src/MyProject
+dotnet sln add src/MyProject/MyProject.fsproj
 mkdir tests
-dotnet new xunit -lang F# -o tests/Ch04Tests
-dotnet sln add tests/Ch04Tests/Ch04Tests.fsproj
-cd tests/Ch04Tests
-dotnet add reference ../../src/Ch04/Ch04.fsproj
+dotnet new xunit -lang F# -o tests/MyProjectTests
+dotnet sln add tests/MyProjectTests/MyProjectTests.fsproj
+cd tests/MyProjectTests
+dotnet add reference ../../src/MyProject/MyProject.fsproj
 dotnet add package FsUnit
 dotnet add package FsUnit.XUnit
 dotnet build
