8aa29fc4e98bd31d168b01c7ed6f2254d41273eb
laj <larry.jones@reveal-energy.com>
2022-06-13 20:53:13 -0500

    Final customer tests

--- Ch04/CustomerSolution/tests/MyProjectTests/CustomerTests.fs
+++ Ch04/CustomerSolution/tests/MyProjectTests/CustomerTests.fs
@@ -7,6 +7,21 @@ open MyProject
 open MyProject.Domain
 
 
+[<AutoOpen>]
+module TestHelpers =
+    let failTest msg =
+        Assert.True(false, msg)
+        
+    let passTest msg =
+        Assert.True(true)
+        
+    let isCustomerAlreadyExistsException message customerId =
+        let expected = $"Customer '{customerId}' already exists"
+        match message with
+        | s when s = expected -> passTest ()
+        | _ -> failTest $"%A{message} not expected"
+
+
 module ``Convert customer to eligible`` =
     let sourceCustomer = { 
         CustomerId = "John";
@@ -25,3 +40,21 @@ module ``Convert customer to eligible`` =
         let upgraded = convertToEligible sourceCustomer
         upgraded |> should equal sourceCustomer
         
+module ``Create customer`` =
+    let name = "John"
+    
+    [<Fact>]
+    let ``should succeed if customer does not exist`` () =
+        let existing = None
+        let result = tryCreateCustomer name existing
+        match result with
+        | Ok customer -> customer |> should equal { CustomerId = name; IsRegistered = true; IsEligible = false }
+        | Error ex -> ex.ToString () |> failTest
+        
+    [<Fact>]
+    let ``should fail if customer does exist`` () =
+        let existing = Some { CustomerId = name; IsRegistered = true; IsEligible = false }
+        let result = tryCreateCustomer name existing
+        match result with
+        | Error ex -> isCustomerAlreadyExistsException ex.Message name
+        | Ok customer -> failTest $"{customer} was not expected"
