af88ca577a35f184404e58d88bec28cacb4850c7
Larry Jones <mrwizard82d1@gmail.com>
2026-01-21 17:13:18 -0600

    Define Customer type and instance

--- ApplyDiscount/Program.fs
+++ ApplyDiscount/Program.fs
@@ -1,2 +1,14 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Define a customer. A customer is a _AND_ type.
+type Customer = {
+    Id: string
+    IsEligible: bool
+    IsRegistered: bool
+}
+
+// Construct an instance of a customer (for example).
+let fred = {
+    Id = "Fred"
+    IsEligible = true
+    IsRegistered = true
+}
+fred
