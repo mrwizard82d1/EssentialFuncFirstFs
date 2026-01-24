9678ec36b44c4ab41b9e9998ad75ed3373b64cb4
Larry Jones <mrwizard82d1@gmail.com>
2026-01-23 18:13:54 -0600

    Implement try-parse function with pattern matching

--- NullAndExceptions/option.fsx
+++ NullAndExceptions/option.fsx
@@ -10,3 +10,12 @@ let tryParseDateTime (input: string) =
 tryParseDateTime "2026-01-23"
 tryParseDateTime "2026-01-23T18:08:27.355"
 tryParseDateTime "20260123T180827.355"
+
+let tryParseDateTimeMatch (input: string) =
+    match DateTime.TryParse input with
+    | true, result -> Some result
+    | _ -> None
+
+tryParseDateTimeMatch "2026-01-23"
+tryParseDateTimeMatch "2026-01-23T18:08:27.355"
+tryParseDateTimeMatch "20260123T180827.355"
