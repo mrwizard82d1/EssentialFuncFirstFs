871c8bd5410661d66d9d45c2f32525b8ae8b2fa7
Larry Jones <mrwizard82d1@gmail.com>
2026-01-24 20:03:44 -0600

    Implement phony db and domain function with error

--- MySolution/src/MyProject/Customer.fs
+++ MySolution/src/MyProject/Customer.fs
@@ -1,26 +1,64 @@
-namespace MyApplication.Customer
-
-// open System
+namespace MyProject
 
 // The `Customer` type is used in two different modules, so it is
 // defined in the scope of the **namespace** and outside **both**
 // modules. Because the modules are defined in the namespace, they
 // have access to the `Customer` type without needing to import it.
 type Customer = {
-    Name : string
+    CustomerId : string
+    IsRegistered : bool
+    IsEligible : bool
 }
-
-module Domain =
-    
-    type Name = string
-    
-    let create (name : Name) =
-        { Name = name }
-        
+            
 module Db =
     
-//    open System.IO
+    let tryGetCustomer customerId =
+        try
+            [
+                {
+                    CustomerId = "John"
+                    IsRegistered = true
+                    IsEligible = true
+                }
+                {
+                    CustomerId = "Mary"
+                    IsRegistered = true
+                    IsEligible = true
+                }
+                {
+                    CustomerId = "Richard"
+                    IsRegistered = true
+                    IsEligible = false
+                }
+                {
+                    CustomerId = "Sarah"
+                    IsRegistered = false
+                    IsEligible = false
+                }
+            ]
+            |> List.tryFind (fun c -> c.CustomerId = customerId)
+            |> Ok
+        with
+        | ex -> Error ex
+        
+    let saveCustomer (customer: Customer) =
+        try
+            // Save customer
+            Ok ()
+        with
+        | ex -> Error ex
+            
+module Domain =
     
-    let save (_customer: Customer) =
-        // Imagine that this function actually talks to a database.
-        ()
+    let convertToEligible customer =
+        if not customer.IsEligible then
+            { customer with IsEligible = true }
+        else
+            customer
+            
+    // This function contains an error that we will fix
+    let upgradeCustomer customerId =
+        customerId
+        |> Db.tryGetCustomer
+        |> convertToEligible
+        |> Db.saveCustomer
