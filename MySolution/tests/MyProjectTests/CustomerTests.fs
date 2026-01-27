4f14e212f91647ce7c982debbcc27ec15c41221c
Larry Jones <mrwizard82d1@gmail.com>
2026-01-27 11:51:53 -0600

    Write unit tests for newly added feature

--- MySolution/tests/MyProjectTests/CustomerTests.fs
+++ MySolution/tests/MyProjectTests/CustomerTests.fs
@@ -5,6 +5,22 @@ open FsUnit
 open MyProject
 open MyProject.Domain
 
+[<AutoOpen>]
+module TestHelpers =
+    
+    let failTest msg =
+        Assert.True(false, msg)
+        
+    let passTest =
+        Assert.True(true)
+        
+    let isCustomerAlreadyExistsException message customerId =
+        let expected = $"Customer '{customerId}' already exists."
+        match message with
+        | s when s = expected ->
+            passTest
+        | _ -> failTest $"%A{message} not expected."
+
 module ``Convert customer to eligible`` =
     
     let sourceCustomer = {
@@ -23,3 +39,25 @@ module ``Convert customer to eligible`` =
     let ``should return eligible customer unchanged`` () =
         let upgraded = convertToEligible sourceCustomer
         upgraded |> should equal sourceCustomer
+        
+    let name = "John"
+    
+    [<Fact>]
+    let ``should succeed if customer does not exist`` () =
+        let existing = None
+        let result = tryCreateCustomer name existing
+        match result with
+        | Ok customer ->
+            customer
+            |> should equal { CustomerId = name; IsRegistered = true; IsEligible = false }
+        | Error ex -> ex.ToString() |> failTest
+        
+    [<Fact>]
+    let ``should fail if customer does exist`` () =
+        let existing = Some { CustomerId = name; IsRegistered = true; IsEligible = false }
+        let result = tryCreateCustomer name existing
+        match result with
+        | Error ex ->
+            isCustomerAlreadyExistsException ex.Message name
+        | Ok customer ->
+            failTest $"{customer} was not expected."
