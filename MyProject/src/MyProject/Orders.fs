bc531c008c5fa7035afb966a923eeda8f99c89d8
Larry Jones <mrwizard82d1@gmail.com>
2026-01-30 12:40:03 -0600

    BROKE I think the code is correct but...
    Apparently, this book is a "previous edition" of the book (now called)
    _Essential F#_ by Ian Russell.
    
    I do not know the full story but I think _Essential Functional-First F#_
    was published (and it is now free from LeanPub) and almost immediately
    revised to _Essential F#_. Sigh...
    
    This theory explains why the "first" book appears to no longer be
    available from LeanPub (nor in Ian Russell's GitHub repository). Sigh...
    
    Back to "the drawing board" (starting over with the "new" book
    _Essential F#_).

--- MyProject/src/MyProject/Orders.fs
+++ MyProject/src/MyProject/Orders.fs
@@ -1,2 +1,49 @@
-module MyProject.Orders
+namespace MyProject
 
+type Item = {
+    ProductId: int
+    Quantity: int
+}
+
+type Order = {
+    Id : int
+    Items : Item list
+}
+    
+module Domain =
+    
+    let addItem item order =
+        // Append item to order
+        let items = item::order.Items
+        // Count how many of each product we have
+        // New product = 1 entry; existing product = 2 entries
+        // Sort items in productId order to simplify equality
+        // Update order with new items
+        { order with Items = items }
+        
+    // Let's create a couple of helpers bindings and some simple
+    // assertions to help us test functionality in FSI.
+    let order = { Id = 1; Items = [ {
+        ProductId = 1
+        Quantity = 1
+        } ]
+    }
+    let newItemExistingProduct = { ProductId = 1; Quantity = 1 }
+    let newItemNewProduct = { ProductId = 2; Quantity = 2 }
+    
+    let t1 = (addItem newItemNewProduct order = {
+        Id = 1
+        Items = [
+            { ProductId = 2; Quantity = 2 }
+            { ProductId = 1; Quantity = 1 }
+        ]
+    })
+    printfn $"{t1}"
+    
+    let t2 = (addItem newItemExistingProduct order = {
+       Id = 1
+       Items= [
+           { ProductId = 1; Quantity = 2 }
+       ]
+    })
+    printfn $"{t2}"
